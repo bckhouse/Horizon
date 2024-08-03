77891f4a3639aa16a6275459aece680bd5a666da
bckhouse <bck.swiss@gmail.com>
2024-08-03 20:15:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import SignIn from './components/auth/SignIn'; // Korrigierter Pfad
-import Home from './components/Home';
-// Weitere Importe, falls vorhanden
+import SignIn from './views/auth/SignIn';
+import Dashboard from './views/Dashboard';
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/home" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
         {/* Weitere Routen */}
         <Redirect from="/" to="/signin" />
       </Switch>
